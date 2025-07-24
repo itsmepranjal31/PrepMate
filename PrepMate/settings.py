45ecf774a42619b71a255ca4b6ac52076b9f0686
unknown <194662856+UtkarshaKusnake@users.noreply.github.com>
2025-07-24 17:56:57 +0530

    Updated CSS and prepared static files for deployment

--- PrepMate/settings.py
+++ PrepMate/settings.py
@@ -117,15 +117,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS= [
     BASE_DIR / 'static'
 ]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"  # where collectstatic puts everything
 
 # STATIC_ROOT=
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
