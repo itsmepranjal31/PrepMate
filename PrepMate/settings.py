e37ece39c05a24bcfc023c4fff476fe9c487466a
unknown <194662856+UtkarshaKusnake@users.noreply.github.com>
2025-07-24 19:26:38 +0530

    Fix

--- PrepMate/settings.py
+++ PrepMate/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['prepmate-3.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 
